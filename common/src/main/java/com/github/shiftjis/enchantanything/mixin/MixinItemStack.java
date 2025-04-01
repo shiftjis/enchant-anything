870b9bbbeff238246c95c2338086e1b7086422d5
Toshimichi0915 <main@toshimichi.net>
2025-04-02 03:08:07 +0900

    Modify ItemStack#getDestroySpeed

--- common/src/main/java/com/github/shiftjis/enchantanything/mixin/MixinItemStack.java
+++ common/src/main/java/com/github/shiftjis/enchantanything/mixin/MixinItemStack.java
@@ -1,6 +1,7 @@
 package com.github.shiftjis.enchantanything.mixin;
 
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,5 +12,14 @@ public class MixinItemStack {
     @Inject(method = "isEnchantable", at = @At("HEAD"), cancellable = true)
     public void isEnchantable(CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(true);
+    }
+
+    @Inject(method = "getDestroySpeed", at = @At("RETURN"), cancellable = true)
+    public void getDestroySpeed(BlockState state, CallbackInfoReturnable<Float> cir) {
+        float currentValue = cir.getReturnValue();
+        if (currentValue == 1.0F) {
+            // reference: net/minecraft/world/entity/player/Player.java.getDestroySpeed
+            cir.setReturnValue(1.01F);
+        }
     }
 }
