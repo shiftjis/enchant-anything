d6aaa9a7056204178bd6dd99fde0371494ef0dc6
Toshimichi0915 <main@toshimichi.net>
2025-04-02 03:01:49 +0900

    Merge duplicating levels

--- common/src/main/java/com/github/shiftjis/enchantanything/mixin/MixinEnchantmentHelper.java
+++ common/src/main/java/com/github/shiftjis/enchantanything/mixin/MixinEnchantmentHelper.java
@@ -2,6 +2,9 @@ package com.github.shiftjis.enchantanything.mixin;
 
 import com.google.common.collect.Lists;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
@@ -14,12 +17,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
+import static net.minecraft.world.item.enchantment.EnchantmentHelper.getEnchantmentId;
+
 @Mixin(EnchantmentHelper.class)
 public class MixinEnchantmentHelper {
     @Inject(method = "getEnchantmentCost", at = @At("HEAD"), cancellable = true)
     private static void getEnchantmentCost(RandomSource random, int enchantNum, int power, ItemStack stack, CallbackInfoReturnable<Integer> cir) {
         int minCost = random.nextInt(8) + 1 + (power >> 1) + random.nextInt(power + 1);
-        minCost = enchantNum == 0 ?  Math.max(minCost / 3, 1) : enchantNum == 1 ? minCost * 2 / 3 + 1 : Math.max(minCost, power * 2);
+        minCost = enchantNum == 0 ? Math.max(minCost / 3, 1) : enchantNum == 1 ? minCost * 2 / 3 + 1 : Math.max(minCost, power * 2);
         cir.setReturnValue(stack.getMaxStackSize() == 1 ? minCost : minCost + (stack.getEnchantmentTags().size() * 3));
     }
 
@@ -32,5 +37,27 @@ public class MixinEnchantmentHelper {
             }
         }
         cir.setReturnValue(enchantmentInstances);
+    }
+
+    @Inject(method = "getItemEnchantmentLevel", at = @At("HEAD"), cancellable = true)
+    private static void getItemEnchantmentLevel(Enchantment enchantment, ItemStack stack, CallbackInfoReturnable<Integer> cir) {
+        if (stack.isEmpty()) {
+            cir.setReturnValue(0);
+            return;
+        }
+
+        ResourceLocation resourcelocation = getEnchantmentId(enchantment);
+        ListTag listtag = stack.getEnchantmentTags();
+
+        int totalLevel = 0;
+        for (int i = 0; i < listtag.size(); ++i) {
+            CompoundTag compoundtag = listtag.getCompound(i);
+            ResourceLocation resourcelocation1 = EnchantmentHelper.getEnchantmentId(compoundtag);
+            if (resourcelocation1 != null && resourcelocation1.equals(resourcelocation)) {
+                totalLevel += EnchantmentHelper.getEnchantmentLevel(compoundtag);
+            }
+        }
+
+        cir.setReturnValue(totalLevel);
     }
 }
